c5ba028e066980b0b7d8da6835deb272ff743199
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-04-29 21:54:04 +0300

    Update matrix.cs

--- matrix.cs
+++ matrix.cs
@@ -1,1 +1,27 @@
-
+// 2. כתוב פונקציה המגדירה מטריצה 4 על 5 מסוג מספרים שלמים. הפונקציה תבדוק ותדפיס כמה מספרים זוגיים קיימים במטריצה 
+using System;
+namespace chapter
+{
+    class chapter1
+    {
+        static void manyDouble()
+        {
+            int i, j;
+            int count = 0;
+            int[,] table = { { 2, 5, 7, 9 }, { 11, 13, 15, 17 }, { 19, 21, 23, 25 } };
+            for(i=0;i<table.GetLength(0);i++)
+            {
+                for (j = 0; j < table.GetLength(1);j++)
+                {
+                    if (table[i, j] % 2 == 0)
+                        count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+        static void Main(string[] args)
+        {
+            manyDouble();
+        }
+    }
+}
