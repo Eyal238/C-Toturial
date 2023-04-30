977ace0b860cf7ee16199a11f8cbcf6d64d5daed
Eyal238 <130982982+Eyal238@users.noreply.github.com>
2023-04-30 09:54:33 +0300

    Update matrix.cs

--- matrix.cs
+++ matrix.cs
@@ -1,3 +1,33 @@
+// 1. כתוב פונקציה הקולטת מהמקלדת מטריצה בגודל 3 על 3 מסוג מספרים שלמים. הפונקציה תדפיס כמה ערכים במטריצה גדולים מ-10 וקטנים מ-100
+
+using System;
+namespace matrix
+{
+    class Program
+    {
+        static void f1(int[,] num)
+        {
+            int i, j;
+            int count = 0;
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    if (num[i, j] > 10 && num[i, j] < 100)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+  }
+        static void Main(string[] args)
+        {
+            int[,] num = { {12,31,50},{30,42,41},{89,32,16}};
+            f1(num);
+        }
+    }
+}
 // 2. כתוב פונקציה המגדירה מטריצה 4 על 5 מסוג מספרים שלמים. הפונקציה תבדוק ותדפיס כמה מספרים זוגיים קיימים במטריצה 
 using System;
 namespace chapter
@@ -26,7 +56,6 @@ namespace chapter
     }
 }
 // 3.  כתוב פונקציה המקבלת מטריצה 2 על 5 מסוג מחרוזת. הפונקציה בודקת האם השם "יוסי"(באנגלית אותיות קטנות) נמצא במטריצה. אם כן תחזיר חיובי ואם לא תחזיר שלילי
-
 using System;
 namespace chapter
 {
